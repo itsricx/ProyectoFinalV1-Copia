037f6afe96f1f38cc3a9e0e4bfb6ad5578e1f1d3
Ricardo Mejías Dorado <rmejias@AULAS.TRIANA>
2019-05-16 13:36:33 +0200

    second commit

---
+++ src/main/java/com/salesianostriana/dam/proyectofinalv1copia/controller/ControllerIndex.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.salesianostriana.dam.proyectofinalv1copia.controller;
+
+/**
+ * @author rmejias
+ *
+ */
+public class ControllerIndex {
+
+	@GetMapping("/index")
+	public String mostrarIndice()
+	
+}
